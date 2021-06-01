37ea3c51ebbd6cc6a4ac7a15ce647af925b5b9f1
Joshua Fletcher <joshua.f1998@gmail.com>
2021-06-01 15:08:44 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  scope.setInterval(countUp() 1000);
+  const counterInteveral = scope.setInterval(countUp() 1000);
 });
 
 function countUp() {
