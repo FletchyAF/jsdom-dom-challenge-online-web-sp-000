9a60669c8209df1c1d4c20b02dd617c835b2dda2
Joshua Fletcher <joshua.f1998@gmail.com>
2021-06-01 15:04:23 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("counter").setInterval(func)
-})
+  document.getElementById("counter").setInterval(function, [1000]);
+)
+});
